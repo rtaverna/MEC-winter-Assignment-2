79983170544b180af4c3e5788e1b90a8b0e7b333
Sean Wilson <hello@imsean.me>
2021-01-06 20:48:46 -0500

    remove old files

--- stand.js
+++
@@ -1,9 +0,0 @@
-import determineOutcome from './determine_outcome'
-import displaySum from './display_sum'
-
-function stand (player_hand, dealer_hand) {
-  displaySum(player_hand, dealer_hand)
-  determineOutcome(player_hand, dealer_hand)
-}
-
-export default stand
